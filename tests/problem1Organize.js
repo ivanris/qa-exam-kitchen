732b73c17f920b8e5d4b8f24c1ac4e276797b191
Ivan Ristic <ivanris28@gmail.com>
2023-01-22 17:39:33 +0100

    initial commit

---
+++ tests/problem1Organize.js
@@ -0,0 +1,109 @@
+import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
+
+const localStorageGet = ClientFunction(key => localStorage.getItem(key));
+
+const qaExamKitchen = Selector("a.navbar-brand")
+const picture = Selector("div.col-md-5 ftco-animate img img-2 fadeInUp ftco-animated")
+const organiser = Selector("input#name").withAttribute("placeholder","Who is organizing birthday")
+const birthdayPerson = Selector("input#name").withAttribute("placeholder","Who is having birthday")
+const ageOfBirthdayPerson = Selector("input#age.form-control")
+const when = Selector("input#date.form-control")
+const atWhatTime = Selector("input#time.form-control")
+const howManyPeopleField = Selector("select#persons.form-control")
+const noOfPersons =howManyPeopleField.find("option")
+const doYouHaveAlergiesYes = Selector("input#alg_y.rd")
+const doYouHaveAlergiesNo = Selector("input#alg_n.rd")
+const doYouHaveAlergiesMaybe = Selector("input#alg_m.rd")
+
+const walnuts = Selector("input#alg1.cb")
+const chestnuts = Selector("input#alg2.cb")
+const fish = Selector("input#alg3.cb")
+const meat = Selector("input#alg4.cb")
+const shrimp = Selector("input#alg5.cb")
+const gluten = Selector("input#alg6.cb")
+const organiseButton = Selector ("a.btn.btn-primary.px-5.py-3")
+const orderConfirmation = Selector("div#ex1.modal")
+
+const organiserCo = Selector("span#orr")
+const birthdayPersonCo = Selector("span#cbr")
+const ageOfBirthdayPersonCo = Selector("span#agr")
+const whenCo = Selector("span#dtr")
+const atWhatTimeCo = Selector("span#tmr")
+const noOfPersonsCo = Selector("span#gur")
+const allergyCo = Selector("span#alr")
+
+
+fixture('Itekako Automatski test')
+    .page('http://10.15.1.204:3000/reserve')
+    
+
+
+test('Problem 1 Organize', async t => {
+    await t
+       .setTestSpeed(0.5)
+       .typeText(organiser, 'Ivan qa automation e')
+       .typeText(birthdayPerson,"Biljana")
+       .typeText(ageOfBirthdayPerson,"43")
+       .typeText(when,'2023-02-02')
+       .typeText(atWhatTime,"18:00")
+       .click(howManyPeopleField)
+       .click(noOfPersons.withText("6"))
+       .click(doYouHaveAlergiesYes)
+       .click(shrimp)
+       .click(organiseButton)
+
+       //confirmation od celebration (potvrdni modal)
+       
+       .expect(orderConfirmation.innerText).contains("Biljana","Ivan qa automation e","43","2023-02-02","18:00","6-10","Yes")
+       .expect (organiserCo.innerText).eql("Ivan qa automation e")
+       .expect(birthdayPersonCo.innerText).eql("Biljana")
+       .expect(ageOfBirthdayPersonCo.innerText).eql("43")
+       .expect(whenCo.innerText).eql("2023-02-02")  
+       .expect(atWhatTimeCo.innerText).eql("18:00")   
+       .expect(noOfPersonsCo.innerText).eql("6-10")
+       .expect(allergyCo.innerText).eql("Yes")
+
+           //local storage check
+       .expect(localStorageGet("Organizer")).eql("Ivan qa automation e")
+       .expect(localStorageGet("Birthday_Person")).eql("Biljana")
+       .expect(localStorageGet("Age")).eql("43")
+       .expect(localStorageGet("Date")).eql("2023-02-02")
+       .expect(localStorageGet("Time")).eql("18:00")
+       .expect(localStorageGet("alergy")).eql("Yes")
+       .expect(localStorageGet("alergies")).eql("Shrimp")
+       
+});
+
+test("Checking all fields", async t =>{
+await t
+//.setTestSpeed(0.1)
+.expect(qaExamKitchen.innerText).contains("QA Exam Kitchen")
+
+.typeText(organiser, 'Ivan qa automation e')
+.typeText(birthdayPerson,"Biljana")
+.typeText(ageOfBirthdayPerson,"43")
+.typeText(when,'2023-02-02')
+.typeText(atWhatTime,"18:00")
+.click(howManyPeopleField)
+.click(noOfPersons.withText("2"))
+.click(howManyPeopleField)
+.click(noOfPersons.withText("6"))
+.click(howManyPeopleField)
+.click(noOfPersons.withText("11"))
+.click(howManyPeopleField)
+.click(noOfPersons.withText("21"))
+.click(doYouHaveAlergiesYes)
+.click(doYouHaveAlergiesNo)
+.click(doYouHaveAlergiesMaybe)
+.click(walnuts)
+.click(chestnuts)
+.click(fish)
+.click(meat)
+.click(shrimp)
+.click(gluten)
+.click(organiseButton)
+
+
+
+})
---
+++ tests/problem2MindClock.js
@@ -0,0 +1,80 @@
+import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
+
+const leto = Selector("button#btn1")
+const zima = Selector("button#btn2")
+const caj = Selector("button#btn3")
+const kafa = Selector("button#btn4")
+const belo = Selector("button#btn5")
+const crno = Selector("button#btn6")
+const slatko = Selector("button#btn7")
+const slano = Selector("button#btn8")
+const kiselo = Selector("button#btn9")
+const ljuto =Selector("button#btn10")
+const kasika = Selector("button#btn11")
+const viljuska = Selector("button#btn12")
+const duboki = Selector("button#btn13")
+const plitki = Selector("button#btn14")
+const voce = Selector("button#btn15")
+const povrce = Selector("button#btn16")
+const koktel = Selector("button#btn17")
+const pivo = Selector("button#btn18")
+const readMYMind = Selector("button#readmymind")
+
+const leviOdgovori = [leto,caj,belo,slatko,kiselo,kasika,duboki,voce,koktel]
+const desniOdgovori = [zima,kafa,crno,slano,ljuto,viljuska,plitki,povrce,pivo]
+
+fixture('Itekako Automatski test2')
+    .page('http://10.15.1.204:3000/questionaire')
+    
+
+
+test('Problem 12 MindClock', async t => {
+    await t
+        .click(leto)
+        .click(caj)
+        .click(belo)
+        .click(slatko)
+        .click(kiselo)
+        .click(kasika)
+        .click(duboki)
+        .click(voce)
+        .click(koktel)
+        .click(readMYMind)
+
+
+})
+test('Problem 12 MindClock', async t => {
+    await t
+        .click(zima)
+        .click(kafa)
+        .click(crno)
+        .click(slano)
+        .click(ljuto)
+        .click(viljuska)
+        .click(plitki)
+        .click(povrce)
+        .click(pivo)
+        .click(readMYMind)
+
+
+})
+test('Problem 12 MindClock array', async t => {
+ 
+    for (let i =0;i <leviOdgovori.length;i++){
+    await t
+   .click(leviOdgovori[i])
+}
+   await t
+      .click(readMYMind)
+})
+
+test('Problem 12 MindClock array', async t => {
+ 
+    for (let i =0;i <desniOdgovori.length;i++){
+    await t
+   .click(desniOdgovori[i])
+}
+   await t
+      .click(readMYMind)
+})
